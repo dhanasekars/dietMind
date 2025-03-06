0f4534d59db5da8dd9ca573373508eef8267a4f3
Dhanasekar Subramaniam <dhanasekars@gmail.com>
2025-03-06 21:07:21 +0530

    fixed path issue

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { OpenAI } from "openai";
 
+import path from "path";
+import { fileURLToPath } from "url"; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 dotenv.config();
 
 const app = express();
